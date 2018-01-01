532f643bc3e244ca3146a0c0451241d5fdab8164
Chris Elliott <ctelliott@gmail.com>
2018-01-01 09:34:15 -0500

    Refactor

--- docker/consul-bu/src/consul_bu.py
+++ docker/consul-bu/src/consul_bu.py
@@ -8,28 +8,37 @@ import logging
 import options
 import requests
 import datetime
-from flask import Flask, jsonify, request, Response
+from flask import Flask, request, Response
 from functools import wraps
+
+options = options.get_options()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
-options = options.get_options()
 
 app = Flask(__name__)
 s3 = boto3.resource('s3',
   aws_access_key_id=options['aws_access_key_id'],
   aws_secret_access_key=options['aws_secret_access_key'],
-  region_name=options['region'])
+  region_name=options['aws_default_region'])
+
+def ok():
+  return json.dumps({'status':'ok'}), 200, {'ContentType':'application/json'}
+
+def err(e):
+  logger.error(e)
+  return json.dumps({'status':'error', 'msg': e}), 500, {'ContentType':'application/json'}
 
 def check_auth(username, password):
   """Uses vault userpass for auth. See readme"""
-  headers = {'content-type': 'application/json'}
   url = "{0}/v1/auth/userpass/login/{1}".format(options['vault_addr'], username)
+  headers = {'content-type': 'application/json'}
   data = { "password": password }
-  r = requests.post(url, headers=headers, data=json.dumps(data))
-  response = json.loads(r.text)
   try:
+    r = requests.post(url, headers=headers, data=json.dumps(data))
+    response = json.loads(r.text)
     if response['auth']['client_token']:
+      logger.info("{} has been authenticated".format(username))
       return True
   except:
     pass
@@ -39,7 +48,7 @@ def authenticate():
   'Login Required', 401,
   {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-def requires_auth(f):
+def auth(f):
   @wraps(f)
   def decorated(*args, **kwargs):
     auth = request.authorization
@@ -48,28 +57,43 @@ def requires_auth(f):
     return f(*args, **kwargs)
   return decorated
 
-def backup_consul():
-  """Gets snapshot from consul http endpoint and copies to s3 bucket"""
-  r = requests.get("{0}/v1/snapshot".format(options['consul_addr']))
-  with open(options['tmp_file'], 'wb') as code:
-    code.write(r.content)
+def upload():
+  """Uploads snapshot to s3"""
   now = datetime.datetime.now()
   key = "consul-bu/{0}-snapshot.tgz".format(now.strftime("%Y-%m-%d"))
-  s3.meta.client.upload_file(options['tmp_file'], options['bucket'], key)
-  os.remove(options['tmp_file'])
+  try:
+    s3.meta.client.upload_file(options['tmp_file'], options['s3_bucket'], key)
+    os.remove(options['tmp_file'])
+except e:
+    err(e)
+    exit(1)
+
+def backup():
+  """Gets snapshot from consul http endpoint"""
+  try:
+    r = requests.get("{0}/v1/snapshot".format(options['consul_addr']))
+    with open(options['tmp_file'], 'wb') as f:
+      f.write(r.content)
+    upload()
+  except e:
+    err(e)
+    exit(1)
 
 @app.route('/healthz', methods=['GET'])
 def healthz():
-  return json.dumps({'status':'ok'}), 200, {'ContentType':'application/json'}
+  return ok()
 
 @app.route('/', methods=['GET'])
-@requires_auth
+@auth
 def trigger():
-  backup_consul()
-  return json.dumps({'status':'ok'}), 200, {'ContentType':'application/json'}
+  backup()
+  return ok()
 
 if __name__ == "__main__":
   while True:
-    backup_consul()
-    app.run(host='0.0.0.0', port=options['port'], debug=options['debug'])
+    backup()
+    try:
+      app.run(host='0.0.0.0', port=options['port'], debug=options['debug'])
+    except:
+      pass
     time.sleep(int(options['interval']))
--- docker/consul-bu/src/options.py
+++ docker/consul-bu/src/options.py
@@ -4,10 +4,10 @@ import os
 
 def get_options():
   return {
-    'bucket': os.getenv('BUCKET', 'cheapandsalty'),
-    'region': os.getenv('REGION', 'us-east-1'),
+    'aws_default_region': os.getenv('AWS_DEFAULT_REGION', 'us-east-1'),
     'aws_access_key_id': os.getenv('AWS_ACCESS_KEY_ID'),
     'aws_secret_access_key': os.getenv('AWS_SECRET_ACCESS_KEY'),
+    's3_bucket': os.getenv('S3_BUCKET'),
     'port': os.getenv('PORT', 3000),
     'debug': os.getenv('DEBUG', False),
     'consul_addr': os.getenv('CONSUL_ADDR', 'http://127.0.0.1:8500'),
